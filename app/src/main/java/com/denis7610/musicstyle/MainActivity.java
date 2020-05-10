deaef5e107e4b998d08ed8a551a82c9c6dd53b71
denispavlenko7610 <denispavlenko7610@gmail.com>
2020-05-10 15:54:26 +0300

    add banner admob

--- app/src/main/java/com/denis7610/musicstyle/MainActivity.java
+++ app/src/main/java/com/denis7610/musicstyle/MainActivity.java
@@ -5,17 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageButton playButton;
+    private ImageButton nextButton;
+    private ImageButton previewButton;
+    boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //adMob
+        adMobCreate();
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        playButton = findViewById(R.id.playButton);
+        playButton = findViewById(R.id.playButton);
+        playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(this);
+    }
+
+    private void adMobCreate() {
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
     }
 
     public void licenseInfo(View view) {
         Intent intent = new Intent(this, LicenseInfo.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
     }
 }
