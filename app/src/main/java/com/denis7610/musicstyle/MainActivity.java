7e75d9fca37c06b04b72cdd9d055fc7e7c96d4b2
denispavlenko7610 <denispavlenko7610@gmail.com>
2020-05-11 23:21:39 +0300

    add music player

--- app/src/main/java/com/denis7610/musicstyle/MainActivity.java
+++ app/src/main/java/com/denis7610/musicstyle/MainActivity.java
@@ -3,50 +3,97 @@ package com.denis7610.musicstyle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView adView;
     private ImageButton playButton;
     private ImageButton nextButton;
     private ImageButton previewButton;
-    boolean isPlaying = false;
+    private SoundPool soundPool;
+    private int[] instrumentalSounds = new int[12];
+    List<Integer> listOfMusic = new ArrayList<>();
+    MediaPlayer mediaPlayer;
+    float volumeInstruments = 0.7f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initComponents();
+
+        createSoundPool();
+
         //adMob
         adMobCreate();
 
-        initComponents();
+        createMediaPlayer();
+    }
+
+    private void createMediaPlayer() {
+        mediaPlayer = MediaPlayer.create(this, listOfMusic.get(51));
+        mediaPlayer.setLooping(true);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.setVolume(1f, 1f);
+    }
+
+    private void createSoundPool() {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+        soundPool = new SoundPool.Builder().setMaxStreams(10)
+                .setAudioAttributes(audioAttributes)
+                .build();
+
+        //sound1 = soundPool.load(this,R.raw.aa_kick,1);
+        for (int i = 0; i < 12; i++) {
+            instrumentalSounds[i] = soundPool.load(this, listOfMusic.get(i), 1);
+        }
     }
 
     private void initComponents() {
         playButton = findViewById(R.id.playButton);
-        playButton = findViewById(R.id.playButton);
-        playButton = findViewById(R.id.playButton);
-        playButton.setOnClickListener(this);
+
+        //add all music
+        for (Field field : R.raw.class.getFields()) {
+            try {
+                listOfMusic.add(field.getInt(field));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void adMobCreate() {
-        AdView adView = new AdView(this);
-        adView.setAdSize(AdSize.BANNER);
-        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 
     public void licenseInfo(View view) {
@@ -54,10 +101,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
     }
 
-    @Override
-    public void onClick(View v) {
+    public void playInstrumentalSound(View v) {
         switch (v.getId()) {
+            case R.id.imageButton:
+                soundPool.play(instrumentalSounds[0], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton2:
+                soundPool.play(instrumentalSounds[1], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton3:
+                soundPool.play(instrumentalSounds[2], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton4:
+                soundPool.play(instrumentalSounds[3], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton5:
+                soundPool.play(instrumentalSounds[4], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton6:
+                soundPool.play(instrumentalSounds[5], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton7:
+                soundPool.play(instrumentalSounds[6], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton8:
+                soundPool.play(instrumentalSounds[7], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton9:
+                soundPool.play(instrumentalSounds[8], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton10:
+                soundPool.play(instrumentalSounds[9], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton11:
+                soundPool.play(instrumentalSounds[10], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+            case R.id.imageButton12:
+                soundPool.play(instrumentalSounds[11], volumeInstruments, volumeInstruments, 0, 0, 1);
+                break;
+        }
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (soundPool != null) {
+            soundPool.release();
+            soundPool = null;
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    public void playButtonClick(View view) {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+            playButton.setBackgroundResource(R.drawable.ic_stop_black_24dp);
+        } else {
+            mediaPlayer.stop();
+            playButton.setBackgroundResource(R.drawable.ic_play_circle);
         }
     }
 }
